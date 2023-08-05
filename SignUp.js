1b0e06dea154e42026dfd7c86c3fd864d835af24
Ciedner <ciednermabale@gmail.com>
2023-08-05 18:04:14 +0800

    wekwek

--- SignUp.js
+++ SignUp.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image, Alert, ScrollView } from 'react-native';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { collection, addDoc } from 'firebase/firestore';
 import { auth, db } from './config/firebase'; 
+import { useNavigation } from '@react-navigation/native';
 
 export function SignupScreen() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [address, setAddress] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+  const [car, setCar] = useState('');
+  const [carPlateNumber, setCarPlateNumber] = useState('');
+
+  const navigation = useNavigation();
 
   const handleSignup = async () => {
     try {
@@ -17,16 +26,25 @@ export function SignupScreen() {
       await addDoc(userRef, {
         name,
         email,
+        address,
+        contactNumber,
+        age,
+        gender,
+        car,
+        carPlateNumber,
       });
 
       console.log('Signup successful!');
+      Alert.alert('Success', 'Successfully registered!', [{ text: 'OK' }]);
+      navigation.navigate('Login');
     } catch (error) {
       console.error('Error signing up: ', error);
+      Alert.alert('Error', 'Registration failed. Please try again.', [{ text: 'OK' }]);
     }
   };
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
          <Image
         source={{
           uri: 'https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-9/364409165_298245242765459_1939857550581027986_n.png?_nc_cat=106&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGH8ceYN0OHIYcmDG7ZPRrgbO7D2w_v0Fds7sPbD-_QV4P_uFjgu3QI2_YGKamA-1PwUOPMWVoEcFSM2q3jFaWo&_nc_ohc=yzRgVQ2QvdUAX90hfND&_nc_ht=scontent.fceb2-1.fna&oh=03_AdSsZ8kD8a0pAH3cUE5zmTWuBKi3fAOrdz-39PExaEWJQg&oe=64EEF0DB',
@@ -56,24 +74,64 @@ export function SignupScreen() {
         onChangeText={setPassword}
         secureTextEntry
       />
+       <TextInput
+        style={styles.input}
+        placeholder="Address"
+        value={address}
+        onChangeText={setAddress}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contact Number"
+        value={contactNumber}
+        onChangeText={setContactNumber}
+        keyboardType="phone-pad"
+      />
+     <View style={styles.genderContainer}>
+        <Text style={styles.label}>Gender:</Text>
+        <TouchableOpacity
+          style={[styles.genderOption, gender === 'male' && styles.selectedGender]}
+          onPress={() => setGender('male')}
+        >
+          <Text style={styles.genderText}>Male</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.genderOption, gender === 'female' && styles.selectedGender]}
+          onPress={() => setGender('female')}
+        >
+          <Text style={styles.genderText}>Female</Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Car"
+        value={car}
+        onChangeText={setCar}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Car Plate Number"
+        value={carPlateNumber}
+        onChangeText={setCarPlateNumber}
+      />
       <TouchableOpacity style={styles.button} onPress={handleSignup}>
         <Text style={styles.buttonText}>Register</Text>
       </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'black',
     padding: 20,
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 100,
+    height: 100,
     resizeMode: 'contain',
     marginBottom: 20,
   },
@@ -104,6 +162,31 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 18,
     fontFamily:'Courier New'
+  },
+  genderContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  label: {
+    marginRight: 10,
+    fontSize: 16,
+    color: 'white',
+  },
+  genderOption: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  selectedGender: {
+    borderColor: '#3b89ac',
+    backgroundColor: '#3b89ac',
+  },
+  genderText: {
+    color: 'white',
   },
 });
 export default SignupScreen;
